bc6287048fe0b10cfccd8f2e4a151d62545515d0
3610cn <maoquan@htsc.com>
2017-02-08 14:20:54 +0800

    1. tabbar的隐藏显示逻辑 2. searchbar in navbar

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,7 @@ import Frame from '../layouts/Frame';
 import TabPane from '../layouts/TabPane';
 
 import ProductHome from '../views/product/Home';
+import ProductDetail from '../views/product/Detail';
 import CustomerHome from '../views/customer/Home';
 import CustomerDetail from '../views/customer/Detail';
 import MissionHome from '../views/mission/Home';
@@ -33,7 +34,10 @@ const routes = history => (
         <IndexRoute component={MissionHome} />
         <Route path=":id" component={MissionDetail} />
       </Route>
-      <Route path="product" components={ProductHome} />
+      <Route path="product" components={TabPane}>
+        <IndexRoute component={ProductHome} />
+        <Route path="detail" components={ProductDetail} />
+      </Route>
       <Route path="customer" component={TabPane}>
         <IndexRoute component={CustomerHome} />
         <Route path=":id" component={CustomerDetail} />
