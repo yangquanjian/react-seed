f683701c1b7563f6f0f54e8080908bc9e55cd88e
sunweibin <695170566@qq.com>
2017-03-02 20:22:10 +0800

    sunweibin:客户搜索结果关键字高亮显示

--- src/components/customer/SearchItem.js
+++ src/components/customer/SearchItem.js
@@ -32,19 +32,13 @@ export default class SearchItem extends PureComponent {
       <Icon type={iconType} />
     );
   }
-
-
-
   render() {
     const { data: { custName, custType }, extra, query } = this.props;
-
     return (
-
       <Item
         thumb={this.renderCustomerIcon(custType)}
         onClick={this.handleClick}
       >
-        {console.log(query)}
         <span dangerouslySetInnerHTML={{ __html: custName.replace(new RegExp(decodeURI(query), 'gu'), `<i style="font-style:normal;color:#da4e40;">${decodeURI(query)}</i>`) }} />
         <Brief>{extra}</Brief>
       </Item>
