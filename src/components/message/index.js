8887a23434fdac53a798809c3f3d3bc9da79c160
3610cn <maoquan@htsc.com>
2017-03-07 11:26:01 +0800

    消息组件优化

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,56 +1,43 @@
-import React, { PropTypes, PureComponent } from 'react';
+/**
+ * @file components/message/index.js
+ *  无相关结果 / 加载展示组件
+ * @author wangjunjun
+ */
+import React, { PropTypes } from 'react';
 
 import './index.less';
 
-export default class Message extends PureComponent {
+const TYPE_NOTFOUND = 'notfound';
+const TYPE_LOADING = 'loading';
 
-  static propTypes = {
-    type: PropTypes.string,
-  };
+const CONFIG = {
+  [TYPE_NOTFOUND]: {
+    text: '没有相关的结果',
+    imgSrc: '/static/img/messageNotfound.png',
+  },
+  [TYPE_LOADING]: {
+    text: '努力加载中...',
+    imgSrc: '/static/img/messageLoading.png',
+  },
+};
 
-  static defaultProps = {
-    type: 'notfound',
-  };
-
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '没有相关的结果',
-      imgSrc: '/static/img/messageNotfound.png',
-    };
-  }
-
-  componentWillMount() {
-    const { type } = this.props;
-    switch (type) {
-      case 'notfound':
-        this.setState({
-          text: '没有相关的结果',
-          imgSrc: '/static/img/messageNotfound.png',
-        });
-        break;
-      case 'loading':
-        this.setState({
-          text: '努力加载中...',
-          imgSrc: '/static/img/messageLoading.png',
-        });
-        break;
-      default:
-        this.setState({
-          text: '没有相关的结果',
-          imgSrc: '/static/img/messageNotfound.png',
-        });
-    }
-  }
-
-  render() {
-    const { imgSrc, text } = this.state;
-    return (
-      <div>
-        <img className="warning-img" src={imgSrc} alt="" />
-        <p className="warning-text">{text}</p>
-      </div>
-    );
-  }
+function Message(props) {
+  const { type } = props;
+  const { imgSrc, text } = CONFIG[type];
+  return (
+    <div>
+      <img className="warning-img" src={imgSrc} alt="" />
+      <p className="warning-text">{text}</p>
+    </div>
+  );
 }
+
+Message.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+Message.defaultProps = {
+  type: 'notfound',
+};
+
+export default Message;
