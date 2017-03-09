a51245c313381db705076001177c3fc711883dec
xuxiaoqin <xuxiaoqin@cienet.com.cn>
2017-03-09 17:48:14 +0800

    [MCRM-Web]: 客户详情与native联调

--- src/components/customer/TaskMenu.js
+++ src/components/customer/TaskMenu.js
@@ -32,14 +32,14 @@ export default class TaskMenu extends PureComponent {
   onLeftClick() {
     const { custId, custNumber, custSor } = this.props;
     // push(`/custBasic/${custNumber}/${custSor}/${custId}`);
-    sendEmail({ custId, custNumber, custSor });
+    sendEmail({ cusId: custId, brokerNumber: custNumber, custType: custSor });
   }
 
   @autobind
   onRightClick() {
     const { custId, custNumber, custSor } = this.props;
     // push(`/custBasic/${custNumber}/${custSor}/${custId}`);
-    processMotTask({ custId, custNumber, custSor });
+    processMotTask({ cusId: custId, brokerNumber: custNumber, custType: custSor });
   }
 
   render() {
